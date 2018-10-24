e9e161496ad8f8498902ad86d158c4a3b6ebdc90
M3g4lodon <32544690+M3g4lodon@users.noreply.github.com>
2018-10-24 16:04:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_splix',
       version='0.0.1',
-      install_requires=['gym','selenium','numpy','matplotlib']
+      install_requires=['gym','selenium','numpy','matplotlib','scipy']
 )
